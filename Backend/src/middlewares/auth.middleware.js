2c0dc2814b1eea62d2cf92e52d7528b2866baf93
Atharva Ajagekar <atharva7ajagekar@gmail.com>
2025-01-29 02:34:55 +0530

    imp changes(file names)

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -2,8 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.models.js";
-//import { Restaurant } from "../models/restaurants.models.js"
-//import { Ngo } from "../models/ngo.models.js"
+import { Restaurant } from "../models/restaurants.models.js"
+import { Ngo } from "../models/ngo.models.js"
 
 export const verifyUserJWT =  asyncHandler(async(req, _, next) => {
     try {
@@ -13,6 +13,42 @@ export const verifyUserJWT =  asyncHandler(async(req, _, next) => {
         }
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = await User.findById(decodedToken?._id).select("-password");
+        if (!user) {
+            throw new ApiError(404, "User not found");
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, "Unauthorized, token not valid");
+    }
+});
+
+export const verifyRestaurantJWT =  asyncHandler(async(req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        if(!token) {
+            throw new ApiError(401, "Unauthorized, token not found");
+        }
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await Restaurant.findById(decodedToken?._id).select("-password");
+        if (!user) {
+            throw new ApiError(404, "User not found");
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, "Unauthorized, token not valid");
+    }
+});
+
+export const verifyNgoJWT =  asyncHandler(async(req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        if(!token) {
+            throw new ApiError(401, "Unauthorized, token not found");
+        }
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await Ngo.findById(decodedToken?._id).select("-password");
         if (!user) {
             throw new ApiError(404, "User not found");
         }
