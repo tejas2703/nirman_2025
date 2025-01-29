a9162705b648c0ce175dbe5f4aa32c0da96b3d12
tejas2703 <tejasshinde2703@gmail.com>
2025-01-29 14:36:19 +0530

    New UI

--- Frontend/src/pages/Homepage/HomePage.jsx
+++ Frontend/src/pages/Homepage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../../Components/HomePage/Header'
-import Login from '../../Components/HomePage/Login'
+import Login from '../../Components/Homepage/Login'
 
 const HomePage = () => {
   return (
